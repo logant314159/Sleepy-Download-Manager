5efe1d2d2d9d0f46e31de5edacf97a07212037a2
Logan Thompson <logant314159@gmail.com>
2021-05-19 17:51:48 -0500

    Restructured extensions and added basic examples.

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
+import os
 import time
 
 # nohup <name> &
 # This will allow you to close the console window.
 
+USER = os.getenv("USER")
+DOWNLOADSPATH = f"/home/{USER}/Downloads"
+
 queryDelay = 1
 
-documentsExt = []
-musicExt = []
-picturesExt = []
-videosExt = []
+extensions = {
+    "Documents": ['.txt', '.odt', '.docx'],
+    "Music":     ['.mp3', '.wav', '.flak', '.ogg'],
+    "Pictures":  ['.jpg', '.jpeg', '.png', '.gif', '.svg', '.webp', '.tiff'],
+    "Videos":    ['.mp4', '.mov', '.avi']
+}
 
 while True:
+    fileList = os.listdir(DOWNLOADSPATH)
+
+    if fileList:
+        for file in fileList:
+            pass
+            # Check extension of file and sort accordingly.
+
     time.sleep(queryDelay)
-    # Check for files in Downloads dir
-        # If files, sort them based off of the Ext lists.
