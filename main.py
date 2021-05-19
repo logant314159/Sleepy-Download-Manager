f6f10a477fd77a0255f87d160799167988062bcb
Logan Thompson <loganthompson@pop-os.localdomain>
2021-05-19 15:44:41 -0500

    Outlined basic workflow.

---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+
+# nohup <name> &
+# This will allow you to close the console window.
+
+queryDelay = 1
+
+documentsExt = []
+picturesExt = []
+musicExt = []
+videosExt = []
+
+while True:
+    time.sleep(queryDelay)
+    # Check for files in Downloads dir
+        # If files, sort them based off of the Ext lists.
